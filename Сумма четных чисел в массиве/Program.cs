6c4aa19b6d0f21e64e571b298bd9af9d96e138e9
kunicind1996@gmail.com <kunicind1996@gmail.com>
2023-02-25 00:15:33 +0700

    Добавьте файлы проекта.

---
+++ Сумма четных чисел в массиве/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите колличество элементов массива: \t");
+int NumberArray = int.Parse(Console.ReadLine());
+int[] NewArray = new int[NumberArray];
+int even_numb = 0;
+for (int i = 0; i < NewArray.Length; i++)
+{
+    Console.Write($"Вы вводите значение массива {i}: \t");
+    NewArray[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("\nВывод массива : \t");
+for (int i = 0; i < NewArray.Length; i++)
+{
+        Console.WriteLine(NewArray[i]);
+        if (NewArray[i] % 2 == 0 )
+    {
+        even_numb += NewArray[i];
+    }
+}
+Console.WriteLine("Сумма четных чисел :\t"+even_numb.ToString());
+Console.ReadLine();
